45265baab6266fae48266fb11ee2658ec531f4e6
Michel Marechal <michelm.marechal@gmail.com>
2018-02-18 20:19:54 -0300

    configured change color on click

--- effects.js
+++ effects.js
@@ -1,22 +1,28 @@
 window.onload = function() {  
   // Global Variables
-  const color = document.getElementById('colorSelector');
-  const widthVal = document.getElementById('width');
-  const heightVal = document.getElementById('height');
-  const button = document.getElementById('botao');
+  const color = document.querySelector('#colorSelector');
+  const widthVal = document.querySelector('#width');
+  const heightVal = document.querySelector('#height');
+  const button = document.querySelector('#botao');
   let currentWidth;
   let currentHeight;
   let currentColor;
 
-  // Build the table
+  // // Build the table
   function builder(){
     currentWidth = widthVal.value;
     currentHeight = heightVal.value;
-    console.log(currentHeight);
+    // Add rows and columns
+    for (let i = 1; i <= currentHeight; i++) {
+      document.querySelector('table').
+    }
+    
+
+
   }
   
-  // Button configuration and grid rules
-  button.onclick = function() {
+  // // Button configuration and grid rules
+  button.onclick = function(event) {
     event.preventDefault();
     if (widthVal.value < 1  || heightVal.value < 1) {
       alert("Width and height must be between 1 and 30");
@@ -24,6 +30,15 @@ window.onload = function() {
       alert("Width and height must be between 1 and 30");
     } else { 
       builder();
+    }
+  }
+
+  // // Change cllicked cell color
+  this.onclick = function() {
+    currentColor = color.value;
+    let clickedEl = this.document.querySelector('td');
+    if (clickedEl !== null) {
+      clickedEl.style.background = color.value;
     }
   }
 }
