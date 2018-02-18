70318817586f0735e09f053f12f6f2b0c9f67b25
Michel Marechal <michelm.marechal@gmail.com>
2018-02-17 22:56:06 -0200

    configuring the button system

--- effects.js
+++ effects.js
@@ -1,4 +1,21 @@
-$(document).ready(function(){
-  // global variables declared
-  const currentColor = document.getElementById("color");
-});
+  // Global Variables
+  const color = document.getElementById('colorSelector');
+  const widthVal = document.getElementById('width');
+  const heightVal = document.getElementById('height');
+  const button = document.getElementById('botao');
+  let currentWidth;
+  let currentHeight;
+  let currentColor;
+
+  // Build the table
+  function builder(){
+
+  }
+  
+  // Button configuration
+  button.onclick = function() {
+    currentWidth = widthVal.value;
+    currentHeight = heightVal.value;
+    console.log(currentHeight);
+    console.log(currentWidth);
+  }
