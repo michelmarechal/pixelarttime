1f14e5bc9eb09f7e57c0e28f519ff024f3fe54f3
Michel Marechal <michelm.marechal@gmail.com>
2018-02-18 16:47:10 -0300

    fixing js

--- effects.js
+++ effects.js
@@ -1,3 +1,4 @@
+window.onload = function() {  
   // Global Variables
   const color = document.getElementById('colorSelector');
   const widthVal = document.getElementById('width');
@@ -9,13 +10,20 @@
 
   // Build the table
   function builder(){
-
-  }
-  
-  // Button configuration
-  button.onclick = function() {
     currentWidth = widthVal.value;
     currentHeight = heightVal.value;
     console.log(currentHeight);
-    console.log(currentWidth);
   }
+  
+  // Button configuration and grid rules
+  button.onclick = function() {
+    event.preventDefault();
+    if (widthVal.value < 1  || heightVal.value < 1) {
+      alert("Width and height must be between 1 and 30");
+    } else if (widthVal.value > 30  || heightVal.value > 30) {
+      alert("Width and height must be between 1 and 30");
+    } else { 
+      builder();
+    }
+  }
+}
